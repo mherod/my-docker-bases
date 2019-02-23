7740dc1b241d3ac46efc4c22cd269f76cc39b4b8
Matthew Herod <matthew.herod@myunidays.com>
2019-02-23 14:18:05 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ fi
 for a in `find . -name "Dockerfile" -execdir pwd \;` ; do
 	name=`echo $a | rev | cut -d'/' -f1 | rev`
 	echo $name
-	docker pull mherod/base-ubuntu-$name
-	docker build -t mherod/base-ubuntu-$name $a
-	docker push mherod/base-ubuntu-$name
+	docker pull $DOCKER_USER/base-ubuntu-$name
+	docker build -t $DOCKER_USER/base-ubuntu-$name $a
+	docker push $DOCKER_USER/base-ubuntu-$name
 done
