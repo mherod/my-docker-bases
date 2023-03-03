209d3a6f0eae9d51d090d8f60ffc979151fe2331
Matthew Herod <matthew.herod@myunidays.com>
2023-03-03 10:55:07 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if which docker-machine ; then 
+if which docker-machine ; then
+	# shellcheck disable=SC2046
 	eval $(docker-machine env)
 fi
-for a in `find . -name "Dockerfile" -execdir pwd \;` ; do
-	name=`echo $a | rev | cut -d'/' -f1 | rev`
-	echo $name
-	docker pull $DOCKER_USER/base-$name || docker pull $DOCKER_USER/base-ubuntu-$name
-	docker build -t $DOCKER_USER/base-$name $a
-	docker push $DOCKER_USER/base-$name
+# shellcheck disable=SC2044
+for a in $(find . -name "Dockerfile" -execdir pwd \;) ; do
+	name=$(echo "$a" | rev | cut -d'/' -f1 | rev)
+	echo "$name"
+	docker pull "$DOCKER_USER/base-$name" || docker pull "$DOCKER_USER/base-ubuntu-$name"
+	docker build -t "$DOCKER_USER/base-$name" "$a"
+	docker push "$DOCKER_USER/base-$name"
 done
